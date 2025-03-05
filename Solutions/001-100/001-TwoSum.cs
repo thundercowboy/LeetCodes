168df56523172556ccb4b50f5088a1a9c6e57862
raphaelpedroso <raphael.pedroso97@hotmail.com>
2025-03-05 17:13:44 -0300

    Add: Ex TwoSum

---
+++ Solutions/001-100/001-TwoSum.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        
+        var dict = new Dictionary<int, int>();
+
+        for ( int i = 0; i < nums.Length; i++ ) {
+
+            if (dict.ContainsKey(nums[i])) {
+                return new int[] {
+                    dict[nums[i]], i
+                };
+            } else {
+                dict[target - nums[i]] = i;
+            }
+        }
+        
+        return new int[] { };
+    }
+}
